e08281c5989cb90f881db3bc59e0583f768b5796
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2025-12-01 04:14:22 +0530

    changed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,16 +10,16 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Mehul Gupta")}
+          {emoji("Made with ❤️")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href="https://github.com/mehul-gupta24/Website"
             target="_blank"
             rel="noreferrer"
           >
-            developerFolio
+            Mehul Gupta
           </a>
         </p>
       </div>
